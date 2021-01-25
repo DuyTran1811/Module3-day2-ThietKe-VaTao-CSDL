211735b1d7c2ed5500648d1b1637389efe26d81c
Trần Công Duy <duytran@Trans-MacBook-Pro.local>
2021-01-25 23:12:41 +0700

    update

---
+++ BT2.sql
@@ -0,0 +1,71 @@
+CREATE DATABASE `BT2`;
+USE `BT2`;
+
+CREATE TABLE `customer`(
+`customerNumber` INT NOT NULL AUTO_INCREMENT,
+`customerName` VARCHAR(50)NOT NULL,
+`contactLatsName` VARCHAR(50) NOT NULL,
+`contactFirstName` VARCHAR(50) NOT NULL,
+`phone` VARCHAR(50) NOT NULL,
+`addressLine1` VARCHAR(50) NOT NULL,
+`addssLine2` VARCHAR(50)NULL,
+`city` VARCHAR(50)NOT NULL,
+`state` VARCHAR(50)NOT NULL,
+`postalCode` VARCHAR(15)NOT NULL,
+`counttry` VARCHAR(50)NOT NULL,
+`creaditLimit`FLOAT NULL,
+PRIMARY KEY (`customerNumber`));
+
+CREATE TABLE `orders`(
+`orderNumber` INT NOT NULL AUTO_INCREMENT,
+`orderDate` DATE NOT NULL,
+`requiredDate` DATE NOT NULL,
+`shippedDate` DATE NOT NULL,
+`status` VARCHAR(15)NOT NULL,
+`comments` TEXT NOT NULL,
+`quantityOrdered` INT NOT NULL,
+`priceEach`FLOAT NOT NULL, 
+ PRIMARY KEY (`orderNumber`));
+
+CREATE TABLE `payments`(
+`customer_Number` INT NOT NULL AUTO_INCREMENT,
+`checkNumber` VARCHAR(50)NOT NULL,
+`paymentDate` DATE NOT NULL,
+`amount` FLOAT NOT NULL,
+PRIMARY KEY(`customer_Number`));
+
+CREATE TABLE `products`(
+`productCode` VARCHAR(15)NOT NULL,
+`productName` VARCHAR(70)NOT NULL,
+`productScale` VARCHAR(10) NOT NULL,
+`productVendor` VARCHAR(50) NOT NULL,
+`productDescription` TEXT NOT NULL,
+`quantityInSrock`INT NOT NULL,
+`buyPrice` FLOAT NOT NULL,
+`MSRP` FLOAT NOT NULL);
+
+CREATE TABLE `prodictLine`(
+`productLine` VARCHAR(50)NOT NULL,
+`textSesription` VARCHAR(255) NULL,
+`image` VARCHAR(255)NOT NULL);
+
+CREATE TABLE `employee`(
+`employeeNumber` INT NOT NULL AUTO_INCREMENT,
+`lastName` VARCHAR(50) NOT NULL,
+`firstName` VARCHAR(50) NOT NULL,
+`email` VARCHAR(100) NOT NULL,
+`jbTitle` VARCHAR(50) NOT NULL,
+PRIMARY KEY(`employeeNumber`));
+
+CREATE TABLE `offices`(
+`officeCode` VARCHAR(10) NOT NULL,
+`city` VARCHAR(50) NOT NULL,
+`phone` VARCHAR(50) NOT NULL,
+`addressLine1` VARCHAR(255) NOT NULL,
+`addressLine2` VARCHAR(255) NULL,
+`state` VARCHAR(50) NOT NULL,
+`country` VARCHAR(50) NOT NULL,
+`postalCode` VARCHAR(15)NOT NULL);
+
+
+
